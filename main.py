90afa182ca44ae8d49f24b8ad4ae9146cea838f1
zhang-shuning <ningnongidk@gmail.com>
2026-08-08 05:26:24 -0400

    finished screen scrolling

--- main.py
+++ main.py
@@ -12,13 +12,25 @@ pygame.event.set_blocked(pygame.MOUSEMOTION)
 pygame.mouse.set_cursor(*pygame.cursors.arrow)
 clock = pygame.time.Clock()
 
-surface_list:list[ScreenSurface] = []
+surface_list:list[DynamicScreenSurface] = []
 collider_list:list[ScreenSurface] = []
 screen_offset:int = 0
 screen_end:int = 0
 
+def set_collider(ss:ScreenSurface):
+    '''Sets the ss to a collider and a surface'''
+    collider_list.append(ss)
+    collider_list.append(ss)
+
+def set_surface(ss:ScreenSurface):
+    '''Sets the ss to a surface'''
+    surface_list.append(ss)
+
 class ScreenSurface():
-    '''A surface that gets drawn onto the screen'''
+    '''
+    A surface that gets drawn onto the screen that gets scrolls off the screen.
+    Drawn around a rect, which the hitbox flag shows
+    '''
     def __init__(self, surface:pygame.Surface, rect:pygame.Rect, enabled = True, hitbox_enabled=False) -> None:
         self.surface = surface
         self.rect = rect
@@ -31,11 +43,19 @@ class ScreenSurface():
         '''Draws onto the screen'''
         if not self.enabled:
             return
-        screen.blit(source=self.surface, dest=self.rect)
+        #Check if it's not on the screen to the right
+        if self.rect.x + self.rect.width < screen_offset:
+            return
+        #Check if it's not on the screen to the left
+        if self.rect.x > screen_offset + HORIZONTAL_SIZE:
+           return
+        screen.blit(source=self.surface,
+                    dest=(self.rect.x - screen_offset, self.rect.y))
         if self.hitbox_enabled:
             pygame.draw.rect(surface=screen,
                             color=(255, 0, 0), 
-                            rect=(self.rect),
+                            rect=(self.rect.x - screen_offset, self.rect.y,
+                            self.rect.width, self.rect.height),
                             width=1)
 
 class Player(ScreenSurface):
@@ -46,6 +66,20 @@ class Player(ScreenSurface):
         self.y_vel = 0
         self.x_pos = 0
         self.y_pos = 0
+
+    @override
+    def draw(self):
+        '''Draws onto the screen'''
+        if not self.enabled:
+            return
+        screen.blit(source=self.surface,
+                    dest=(self.rect.x - screen_offset, self.rect.y))
+        if self.hitbox_enabled:
+            pygame.draw.rect(surface=screen,
+                            color=(255, 0, 0), 
+                            rect=(self.rect.x - screen_offset, self.rect.y,
+                            self.rect.width, self.rect.height),
+                            width=1)
 
     def move(self):
         '''
@@ -68,6 +102,7 @@ class Player(ScreenSurface):
             self.rect.y = collider_list[y_intersect_index].y
 
 player = Player(surface=pygame.image.load("assets/temphole.png"), rect=pygame.Rect((0, 0), (16, 16)), hitbox_enabled=True)
+set_surface(ScreenSurface(surface=pygame.image.load("assets/temphole.png"), rect=pygame.Rect((400, 100), (16, 16)), hitbox_enabled=True))
 
 while running:
     clock.tick(60)
@@ -82,9 +117,15 @@ while running:
     #Player moves
     player.move()
     #Offset is updated and everything is drawn
+    relative_screen_distance = player.x_pos - screen_offset
     #Scroll right
-    if player.x_pos - screen_offset > HORIZONTAL_SIZE/2:
-        screen_offset =  
+    if relative_screen_distance > HORIZONTAL_SIZE//2:
+        screen_offset = player.x_pos - HORIZONTAL_SIZE//2
+    #Scroll left
+    if relative_screen_distance < HORIZONTAL_SIZE//3:
+        screen_offset = player.x_pos - HORIZONTAL_SIZE//3
+        if screen_offset < 0:
+            screen_offset = 0
     #Player is a surface but it's done seperately so that the player does not to be readded
     player.draw()
     for i in surface_list:
