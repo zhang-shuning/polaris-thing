5d0e5dbc450b21043477cccf5b9b128dbe25b535
ivanding3 <ivanding3@gmail.com>
2026-08-08 02:30:32 -0400

    added background + cursor

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import pygame
 
 running = True
 pygame.init()
-screen = pygame.display.set_mode((320,180))
-
+screen = pygame.display.set_mode((320,180),vsync=1,flags = pygame.FULLSCREEN|pygame.SCALED)
+pygame.event.set_blocked(pygame.MOUSEMOTION)
+pygame.mouse.set_cursor(*pygame.cursors.arrow)
 while running:
+    
+    # event queue
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    # drawing
+    screen.fill((50,50,50))
+
+    # updates the screen
+    pygame.display.flip()
