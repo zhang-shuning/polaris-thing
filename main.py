140f963b44e1147ea3178686cf2011d844d8e0c7
ivanding3 <ivanding3@gmail.com>
2026-08-08 02:42:13 -0400

    added pixel for refrence

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ while running:
             running = False
     # drawing
     screen.fill((50,50,50))
-
+    pygame.draw.circle(screen,(0,0,0),pygame.mouse.get_pos(),1)
     # updates the screen
     pygame.display.flip()
