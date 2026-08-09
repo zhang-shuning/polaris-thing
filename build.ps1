03c8c879e8c3cc677ea1e97935f355c2eb33bdbb
zhang-shuning <ningnongidk@gmail.com>
2026-08-09 09:08:59 -0400

    Build script

---
+++ build.ps1
@@ -0,0 +1,1 @@
+python -m nuitka --standalone --windows-console-mode=disable --deployment --include-data-dir=assets=assets,maps=maps --python-flag=isolated,no_asserts,no_docstrings,no_warnings --main=main.py
